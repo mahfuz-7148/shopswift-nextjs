535ea52107ba0991884653a100c34b1a6bcb7662
mahfuz-7148 <mrahman7148@gmail.com>
2025-09-06 17:38:48 +0600

    navbar added

---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CheckOutPage() {
+    return (
+        <div>CheckOutPage</div>
+    )
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from "next/link";
+
+export const Navbar = () => {
+    return (
+            <nav className='sticky top-0 z-50 bg-white shadow'>
+                <div className='container mx-auto flex items-center justify-between px-4 py-4'>
+                    <Link href='/' className='hover:text-blue-600'>ShopSwift</Link>
+                </div>
+                <div className='hidden md:flex space-x-6'>
+                    <Link href='/'>Home</Link>
+                    <Link href='/products' className='hover:text-blue-600'>Products</Link>
+                    <Link href='/checkout' className='hover:text-blue-600'>Checkout</Link>
+                </div>
+                <div className='flex items-center space-x-4'></div>
+            </nav>
+    )
+}
