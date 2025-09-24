ce14d17263ebb4a28fa54f81e25a5fa798aafbe3
mahfuz-7148 <mrahman7148@gmail.com>
2025-09-24 15:07:45 +0600

    implement search and Ui design

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,16 +1,31 @@
+'use client'
 import Stripe from "stripe";
 import {ProductCard} from "@/components/product-card";
+import {useState} from "react";
 
 interface Props {
     products: Stripe.Product[]
 }
 export const ProductList = ({products}: Props) => {
+    const [searchTerm, setSearchTerm] = useState<string>('')
+    const filteredProduct = products.filter(product => {
+        const term = searchTerm.toLowerCase()
+        const nameMatch = product.name.toLowerCase().includes(term)
+        const descriptionMatch = product.description? product.description.toLowerCase().includes(term) : false
+        return nameMatch || descriptionMatch
+    })
     return (
         <div>
             <div>
-                <input type="text" placeholder='Search Products'/>
+                <input
+                    type="text"
+                    placeholder='Search Products'
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                    className='w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                />
             </div>
-            <ul>
+            <ul className='mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {products.map((product, key) => {
                     return (
                         <li key={key}>
