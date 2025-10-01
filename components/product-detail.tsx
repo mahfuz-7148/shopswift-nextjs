2615be002bdf83302714c2282e410484235291cd
mahfuz-7148 <mrahman7148@gmail.com>
2025-10-01 17:07:41 +0600

    added cart in details page

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -3,12 +3,25 @@
 import Stripe from "stripe";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {useCartStore} from "@/store/cart-store";
 
 interface Props {
     product: Stripe.Product
 }
 export const ProductDetail = ({product}: Props) => {
     const price = product.default_price as Stripe.Price
+    const {items, addItem, removeItem} = useCartStore()
+    const cartItem = items.find((item) => item.id === product.id);
+    const quantity = cartItem ? cartItem.quantity : 0;
+    const onAddItem = () => {
+        addItem({
+            id: product.id,
+            name: product.name,
+            price: price.unit_amount as number,
+            imageUrl: product.images ? product.images[0] : null,
+            quantity: 1,
+        });
+    };
     return (
         <div className='container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center'>
             {product.images && product.images[0] && (
@@ -29,10 +42,10 @@ export const ProductDetail = ({product}: Props) => {
                 {price && price.unit_amount && (
                     <p>${(price.unit_amount / 100).toFixed(2)}</p>
                 )}
-                <div>
-                    <Button variant='outline'>-</Button>
-                    <span>quanity</span>
-                    <Button>+</Button>
+                <div className='flex items-center space-x-4'>
+                    <Button variant='outline' onClick={() => removeItem(product.id)}>-</Button>
+                    <span className='text-lg font-semibold'>{quantity}</span>
+                    <Button onClick={onAddItem}>+</Button>
                 </div>
             </div>
         </div>
